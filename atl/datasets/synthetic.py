56932084d14f849a34400f67ce27b3ff9ab3efee
natsu0317 <mineno-natsuko686@g.ecc.u-tokyo.ac.jp>
2025-05-15 02:08:16 +0900

    dataset修正

--- atl/datasets/synthetic.py
+++ atl/datasets/synthetic.py
@@ -4,15 +4,15 @@ from sklearn.datasets import make_moons
 
 def create_moon_dataset_with_clusters():
     # main: moon形状 + sub: moon形状 * 2
-    X_main, y_main = make_moons(n_samples=2000, noise=0.1, random_state=42)
+    X_main, y_main = make_moons(n_samples=2000, noise=0.15, random_state=42)
 
-    X_cluster1, y_cluster1 = make_moons(n_samples=250, noise=0.05, random_state=42)
+    X_cluster1, y_cluster1 = make_moons(n_samples=250, noise=0.08, random_state=42)
     X_cluster1 = X_cluster1 * 0.5 - np.array([2.0, 2.0]) # スケールと位置の調整
-    # y_cluster1 = 1 - y_cluster1 # ラベル反転
+    y_cluster1 = 1 - y_cluster1 # ラベル反転
 
-    X_cluster2, y_cluster2 = make_moons(n_samples=250, noise=0.05, random_state=42)
+    X_cluster2, y_cluster2 = make_moons(n_samples=250, noise=0.08, random_state=42)
     X_cluster2 = X_cluster2 * 0.5 - np.array([2.0, 2.0]) # スケールと位置の調整
-    # y_cluster2 = 1 - y_cluster2 # ラベル反転
+    y_cluster2 = 1 - y_cluster2 # ラベル反転
 
     X = np.vstack([X_main, X_cluster1, X_cluster2])
     y = np.hstack([y_main, y_cluster1, y_cluster2])
